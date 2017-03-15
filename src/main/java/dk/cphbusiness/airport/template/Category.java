9d1eb7717f8e445f6f05f975aee0de50e27c0551
Daniel Grønbjerg <danielgronbjerg@gmail.com>
2017-03-15 10:32:58 +0100

    Assignment done to the point where it has arrayqueue implementation that works (hopefully) and has a list of consumers that scale as the queue grows or diminishes

--- src/main/java/dk/cphbusiness/airport/template/Category.java
+++
@@ -1,9 +0,0 @@
-package dk.cphbusiness.airport.template;
-
-public enum Category implements Comparable<Category>{
-    LateToFlight,
-    BusinessClass,
-    Disabled,
-    Family,
-    Monkey;
-  }
